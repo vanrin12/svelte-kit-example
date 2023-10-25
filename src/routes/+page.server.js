48700f84863d8ab1b337905447fb44a304d63ac6
rinto <rinto0503@gmail.com>
2023-10-25 14:41:27 +0700

    Updated

---
+++ src/routes/+page.server.js
@@ -0,0 +1,16 @@
+export const config = {
+    runtime: 'nodejs18.x',
+    isr: {
+        expiration: 10
+    }
+}
+
+export async function load({ }) {
+    const res = await fetch('https://randomuser.me/api/')
+    const data = await res.json();
+    console.log(data);
+    const user = data.results[0];
+    return user;
+}
+
+export const prerender = true;
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let data;
+    $:user = data?.name.first + " " + data?.name.last
+</script>
+<h1>Sveltekit check ISL</h1>
+{#if user}
+<h1>hello {user}</h1>
+{:else}
+<h1>Error</h1>
+{/if}
